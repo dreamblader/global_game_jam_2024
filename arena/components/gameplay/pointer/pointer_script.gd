10e21899840f57a2d84f0b967360c9ea854b7f84
Carlos Gabriel Monnazzi <carlos.monnazzi@datum.inf.br>
2024-01-24 22:16:46 -0300

    refactor code to pointer class added hit signal and crit distance checker

--- arena/components/gameplay/pointer/pointer_script.gd
+++ arena/components/gameplay/pointer/pointer_script.gd
@@ -1,4 +1,53 @@
 extends Area2D
 class_name Pointer
 
-@onready var tip:Node2D = $Tip
+@export var action_name: String = ""
+@export var speed: float = 25.0
+@export var init_position: Vector2 = Vector2(0,0)
+@export var init_rotation_degrees: float = 0
+@export var critical_distance: float = 3
+
+@onready var tip: Node2D = $Tip
+
+signal hit(critical:bool)
+
+
+func _ready() -> void:
+	reset()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(action_name):
+		check_correct_input()
+
+
+func get_angular_speed() -> float:
+	return speed/tip.position.y
+
+
+func move(delta:float, direction:Vector2) -> void:
+	self.position += delta*speed*direction
+
+
+func move_circular(delta:float, clockwise:bool=true) -> void:
+	var inversion: int = 1 if clockwise else -1
+	self.rotation += delta*get_angular_speed()*inversion
+
+
+func check_correct_input() -> void:
+	var correct_distance: float = check_pointer_distance_prompt(get_overlapping_areas())
+	print(correct_distance)
+	if correct_distance >= 0:
+		emit_signal("hit", correct_distance<critical_distance)
+
+
+func check_pointer_distance_prompt(areas: Array[Area2D]) -> float:
+	for area in areas:
+		if area.is_in_group("prompt"):
+			return tip.global_position.distance_to(area.global_position)
+	return -1
+
+
+func reset() -> void:
+	self.position = init_position
+	self.rotation_degrees = init_rotation_degrees
--- arena/components/gameplay/tracker/buttton_tracker_script.gd
+++ arena/components/gameplay/tracker/buttton_tracker_script.gd
@@ -1,49 +1,25 @@
 extends Node2D
 
-@export var pointer_a_speed: float = 25.0
-@export var pointer_b_speed: float = 25.0
+@export var player_id:int = 1
 @export var prompt_size: float = 20.0
 @export var loop_point: float = 180.0
-@export var init_degress:float = 20.0
 
 @onready var pointer_a: Pointer = $PointerA
 @onready var pointer_b: Pointer = $PointerB
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("tickle_a"):
-		check_correct_input(pointer_a)
-	
-	if event.is_action_pressed("tickle_b"):
-		check_correct_input(pointer_b)
+
+func _ready() -> void:
+	pass
 
 
 func _process(delta:float) -> void:
-	move_pointer(pointer_a, delta, pointer_a_speed, false)
-	move_pointer(pointer_b, delta, pointer_b_speed, true)
+	move_pointer(pointer_a, delta, true)
+	move_pointer(pointer_b, delta, false)
 
 
-func move_pointer(pointer: Pointer, delta:float, speed:float, inverse:bool) -> void:
-	var inversion: int = -1 if inverse else 1
+func move_pointer(pointer: Pointer, delta:float, clockwise:bool) -> void:
+	var inversion: int = 1 if clockwise else -1
+	pointer.move_circular(delta, clockwise)
 	
-	pointer.rotation_degrees += speed*delta*inversion
 	if pointer.rotation_degrees*inversion >= loop_point:
-		pointer.rotation_degrees = init_degress*inversion
-
-
-func check_correct_input(pointer: Pointer) -> void:
-	print(check_pointer_distance_prompt(pointer, pointer.get_overlapping_areas()))
-
-
-func check_pointer_distance_prompt(pointer:Pointer, areas: Array[Area2D]) -> float:
-	for area in areas:
-		if area.is_in_group("prompt"):
-			return pointer.tip.global_position.distance_to(area.global_position)
-	return -1
-
-
-func _on_pointer_a_area_entered(area: Area2D) -> void:
-	print(check_pointer_distance_prompt(pointer_a, [area]))
-
-
-func _on_pointer_a_area_exited(area: Area2D) -> void:
-	print(check_pointer_distance_prompt(pointer_a, [area]))
+		pointer.reset()
