6b326c00364277135d6edc5e45f0843a7088b57b
Carlos Gabriel Monnazzi <carlos.monnazzi@datum.inf.br>
2024-02-10 22:13:11 -0300

    added touch buttons

--- arena/components/gameplay/buttons/butons_script.gd
+++ arena/components/gameplay/buttons/butons_script.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var player_id: int
 @export var circle_texture: Array[CompressedTexture2D]
 @export var square_texture: Array[CompressedTexture2D]
 @export var label_circle: String
@@ -9,12 +10,18 @@ extends Node2D
 @onready var square_sprite: Sprite2D = $SquareButtonNormal
 @onready var circle_text: Label = $CircleButtonNormal/LabelCircle
 @onready var square_text: Label = $SquareButtonNormal/LabelSquare
+@onready var circle_touch:TouchScreenButton = $CircleButtonNormal/CircleTouch
+@onready var square_touch:TouchScreenButton = $SquareButtonNormal/SquareTouch
 
 var lock:bool = false
 
 func _ready() -> void:
+	var tickle_a:String = "tickle_a"
+	var tickle_b:String = "tickle_b"
 	circle_sprite.texture = circle_texture[0]
 	square_sprite.texture = square_texture[0]
+	circle_touch.action = tickle_a+str(player_id)
+	square_touch.action = tickle_b+str(player_id)
 	circle_text.text = label_circle
 	square_text.text = label_square
 
@@ -54,3 +61,19 @@ func change_button(button_id:int, press: bool) -> void:
 			2:
 				square_sprite.texture = square_texture[texture_index]
 				square_text.position.y = label_position
+
+
+func _on_circle_touch_pressed() -> void:
+	change_button(1, true)
+
+
+func _on_circle_touch_released() -> void:
+	change_button(1, false)
+
+
+func _on_square_touch_pressed() -> void:
+	change_button(2, true)
+
+
+func _on_square_touch_released() -> void:
+	change_button(2, false)
